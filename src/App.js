38211f998546620cc782f650003769608df33109
youssif hany <youssifhany75@gmail.com>
2021-12-16 01:35:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Features, Footer, Header, Apps } from "./containers";
+import { Navbar } from "./components";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Header />
+      <Apps />
+      <Features />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./header/Header";
+export { default as Features } from "./features/Features";
+export { default as Apps } from "./apps/Apps";
+export { default as Footer } from "./footer/Footer";
